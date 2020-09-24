624c61ca3a0f54be558eee22baff9e23248e0ec9
Falko Benthin <f.benthin@jpberlin.de>
2020-09-24 09:35:25 +0200

    test

--- pages/seminartyp.php
+++ pages/seminartyp.php
@@ -8,7 +8,7 @@
   //Seminartypen auflisten
   if ($func == '') 
   {
-    $query = 'SELECT typ_id, lang_id, bezeichnung FROM ' . rex::getTablePrefix() .'skh3_seminartyp_lok ORDER BY typ_id, lang_id ;' ;
+    $query = 'SELECT typ_id, lang_id, bezeichnung FROM ' . rex::getTablePrefix() .'skh3_seminartyp_lok ORDER BY typ_id, lang_id ' ;
     //echo $query ;
     $list = rex_list::factory($query) ;
 //		$thIcon = '<a href="'. $list->getUrl(array('func' => 'add')) .'"><img src="media/document_plus.gif" alt="add" title="add" /></a>' ;
