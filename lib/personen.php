74b6d7d15969dad6518ba836781cbf061c446d6f
Falko Benthin <f.benthin@jpberlin.de>
2020-09-28 11:43:01 +0200

    personen

--- lib/personen.php
+++ lib/personen.php
@@ -26,16 +26,6 @@ class personen
   private $name ;
 
   /**
-   * @private integer $lang_id Sprach_ID
-   */
-  private $lang_id ;
-
-  /**
-   * @private string $beschreibung Kurzbeschreibung einer Person
-   */
-  private $beschreibung ;
-	
-  /**
    * @access  public
    * @param   integer   $person_id
    */
@@ -45,7 +35,6 @@ class personen
     {
       //echo "PERSON ID\n";
       $this->person_id = $person_id ;
-      $this->lang_id = $lang_id ;
       $sql = \rex_sql::factory() ;
       $sql->setDebug = \rex::getProperty('debug') ;
       //Vornamen und Namen holen, sind in allen Sprachen gleich
@@ -57,20 +46,10 @@ class personen
         $this->name = htmlspecialchars_decode($sql->getValue('name'),ENT_QUOTES) ;
       }
       //holt beschreibung in jeweiliger Sprache
-      $queryBeschreibung = 'SELECT beschreibung FROM ' . \rex::getTablePrefix() . 'skh3_personen_lok WHERE person_id=' . $this->person_id . ' AND lang_id=' . $this->lang_id ;
-      unset($sql) ;
-      $sql = \rex_sql::factory() ;
-      $sql->setDebug = \rex::getProperty('debug') ;
-      $sql->setQuery($queryBeschreibung) ;
-      if($sql->getRow())
-        $this->beschreibung = htmlspecialchars_decode($sql->getValue('beschreibung'),ENT_QUOTES) ;
-    }
     else
     {
       $this->vorname = '' ;
       $this->name = '' ;
-      $this->lang_id = $lang_id ;
-      $this->beschreibung = '' ;
     }
   }
 
@@ -87,14 +66,6 @@ class personen
   {
     return $this->name ;
   }
-  public function getClang()
-  {
-    return $this->lang_id ;
-  }
-  public function getBeschreibung()
-  {
-    return $this->beschreibung;
-  }
 
   //Setter
   public function setVorname($vorname)
@@ -104,15 +75,6 @@ class personen
   public function setName($name)
   {
     $this->name = $name ;
-  }
-  //lokalisierte Beschreibungen
-  public function setClang($lang_id)
-  {
-    $this->lang_id = $lang_id ;
-  }
-  public function setBeschreibung($beschreibung)
-  {
-    $this->beschreibung = $beschreibung ;
   }
 
   //Speichern
@@ -164,58 +126,6 @@ class personen
     return true ;
   }
 	
-  //Speichert Beschreibung zu einer Person
-  public function personBeschreibungSave()
-  {
-    if(empty($this->beschreibung))
-    {
-      echo ('<br />Beschreibung darf nicht leer sein') ;
-      return false ;
-    }
-    $this->beschreibung = htmlspecialchars(trim($this->beschreibung),ENT_QUOTES,'UTF-8') ;
-    // DAtenbank
-    $sql = \rex_sql::factory() ;
-    //prüfen, ob bereits ein Tabelleneintrag existiert -> insert oder update
-    $queryCount = 'SELECT COUNT(*) AS anzahl FROM ' . \rex::getTablePrefix() . 'skh3_personen_lok WHERE person_id = ' . $this->person_id . ' AND langid = ' . $this->langid ;
-    //Query absetzen
-    $sql->setQuery($queryCount) ;
-    //Ergernis prüfen
-    if($sql->getValue('anzahl') != 1) //INSERT
-    {
-      //Namen und Vornamen eintragen
-      echo('Neuer Eintrag \n') ;
-      $queryBeschr = 'INSERT INTO ' . \rex::getTablePrefix() . 'skh3_personen_lok (person_id, lang_id, beschreibung) VALUES (' . $this->person_id . ', ' . $this->lang_id . ', \'' . $this->beschreibung . '\')' ;
-    }
-    else
-    {
-      echo('ändern \n') ;
-      $queryBeschr = 'UPDATE ' . \rex::getTablePrefix() . 'skh3_personen_lok SET beschreibung=\'' . $this->beschreibung . '\' WHERE person_id=' . $this->person_id . ' AND lang_id=' . $this->lang_id ;
-    }
-    if($sql->setQuery($queryBeschr))
-      echo('<br />Beschreibung erfolgreich gespeichert') ;
-    else
-    {
-      echo('<br />Fehler ' .$sql->getError() ) ;
-      return false ;
-    }
-    return true ;
-  }
-	
-  //beschreibung löschen
-  public function beschreibungDelete()
-  {
-    $sql = \rex_sql::factory() ;
-    $sql->setDebug = \rex::getProperty('debug') ;
-    $queryDelete = 'DELETE FROM ' . \rex::getTablePrefix() . 'skh3_personen_lok WHERE person_id=' . $this->person_id .' AND lang_id=' . $this->lang_id ;
-    if($sql->setQuery($queryDelete))
-      echo('<br />Beschreibung gelöscht') ;
-    else
-    {
-      echo('<br />Fehler: ' . $sql->getError() . ' Query: ' . $queryDelete) ;
-      return false ;
-    }
-    return true ;
-  }
 }
 ?>
 
