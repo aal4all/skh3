7923e499bd4f8abc52551e7218055da8563d9fbb
Falko Benthin <f.benthin@jpberlin.de>
2020-09-28 11:42:45 +0200

    personen

--- pages/personen.php
+++ pages/personen.php
@@ -11,17 +11,13 @@
     $tdIcon = '<i class="rex-icon fa-file-text-o"></i>' ;
     $list->addColumn($thIcon, $tdIcon, 0, array( '<th class="rex-icon">###VALUE###</th>', '<td class="rex-icon">###VALUE###</td>' )) ;
     $list->setColumnParams($thIcon, array('func' => 'edit','person_id' => '###person_id###')) ;
-    //Spalte für Beschreibungsicon
-    $tdBesch = '<i class="rex-icon fa-file-text-o"></i>' ;
-    $list->addColumn('Beschr', $tdBesch, 4, array( '<th>###VALUE###</th>', '<td class="rex-icon">###VALUE###</td>' )) ;
-    $list->setColumnParams('Beschr',array('func' => 'desc','person_id' => '###person_id###')) ;
     //spalte zum löschen
     $tdDelete = 'löschen' ;
     $list->addColumn('Löschen', $tdDelete, 5, array( '<th>###VALUE###</th>', '<td class="rex-icon">###VALUE###</td>' )) ;
     $list->setColumnParams('Löschen', array('func' => 'del','person_id' => '###person_id###')) ;
     //id entfernen
     $list->removeColumn('person_id') ;
-    $list->addTableColumnGroup(array(25, '*', '*', 5, 5)) ;
+    $list->addTableColumnGroup(array(5, '*', '*', 5)) ;
     $list->setColumnLabel('vorname', 'Vorname') ; 
     $list->setColumnLabel('name', 'Name') ;
     //felder sortierbar machen
@@ -39,6 +35,33 @@
     if($func == 'edit')
       $person = new skh3\personen($person_id, $lang_id) ;
     //Formular anzeigen
+    $form = rex_form::factory(rex::getTable('skh3_personen'), '', 'person_id=' . rex_request('person_id', 'int', 0), 'post', rex::getProperty('debug')) ;
+    //Start - add firstname-field
+		$field = $form->addTextField('vorname');
+		//$field->setLabel($this->i18n('snippets_label_description'));
+		$field->setLabel('Vorame');
+		$field->getValidator()->add( 'notEmpty', 'Das Feld Name darf nicht leer sein.');
+		//End - add firstname-field
+		//Start - add lastname-field
+		$field = $form->addTextField('name');
+		//$field->setLabel($this->i18n('snippets_label_description'));
+		$field->setLabel('Name');
+		$field->getValidator()->add( 'notEmpty', 'Das Feld Website darf nicht leer sein.');
+		//End - add lastname-field
+		if ($func == 'edit') {
+			$form->addParam('partner_id', $partner_id) ;
+		}
+
+		$content = $form->get() ;
+		$fragment = new rex_fragment() ;
+		$fragment->setVar('class', 'edit', false) ;
+		$fragment->setVar('title', $formLabel, false) ;
+		$fragment->setVar('body', $content, false) ;
+		$content = $fragment->parse('core/page/section.php') ;
+
+		echo $content ;
+    /*
+    // altes formular
     echo('<div class="rex-addon-output">') ;
     $headline = $func == 'edit' ? 'Person bearbeiten' : 'Neue Person' ;
     echo('<h2 class="rex-hl2">' . $headline . ' (ID ' . $person->getPersonID() . ' )</h2>') ;
@@ -61,6 +84,7 @@
       echo('<input type="submit" class="btn" name="submit" value="Speichern" />') ;
       echo('</form></div></div>') ;
     }
+    */
   }
   //person löschen
   if($func == 'del')
@@ -68,70 +92,5 @@
     $person_id = rex_request('person_id', 'int') ;
     $person = new skh3\personen($person_id, $lang_id) ;
     $person->personDelete();
-  }
-  //beschreibungen
-  if($func == 'desc')
-  {
-		$person_id = rex_request('person_id', 'int') ;
-    $query='SELECT person_id, lang_id, beschreibung FROM '. \rex::getTablePrefix() . 'skh3_personen_lok WHERE person_id='.$person_id ;
-    $list = rex_list::factory($query) ;
-    $thIcon = '<a href="'. $list->getUrl(array('func' => 'descAdd','person_id' => $person_id)) .'"><img src="media/document_plus.gif" alt="add" title="add" /></a>' ;
-    $tdIcon = '<img src="media/document.gif" alt="edit" title="edit" />' ;
-    $tdDescDel = 'löschen' ;
-    //Beschreibung bearbeiten
-    $list->addColumn($thIcon, $tdIcon, 0, array( '<th class="rex-icon">###VALUE###</th>', '<td>###VALUE###</td>' )) ;
-    $list->setColumnParams($thIcon, array('func' => 'descEdit','person_id' => '###person_id###', 'lang_id' => '###lang_id###')) ;
-    //Beschreibung löschen
-    $list->addColumn('Löschen', $tdDescDel, 4, array( '<th>###VALUE###</th>', '<td //class="rex-icon">###VALUE###</td>' )) ;
-    $list->setColumnParams('Löschen', array('func' => 'descDel','person_id' => '###person_id###', 'lang_id' => '###lang_id###')) ;
-    //$list->removeColumn('person_id');
-    $list->setColumnLabel('lang_id', 'Sprache') ;
-    $list->setColumnLabel('beschreibung', 'Beschreibung') ;
-    $list->show() ;
-  }
-  //Beschreibungen bearbeiten
-  if($func == 'descAdd' || $func == 'descEdit')
-  {
-    $person = new personen($person_id, $lang_id) ;
-    echo('<div class="rex-addon-output">') ;
-    $headline = $func == 'descEdit' ? 'Personenbeschreibung bearbeiten' : 'Neue Personenbeschreibung' ;
-    echo('<h2 class="rex-hl2">' . $headline . ' (ID ' . $person->getPersonID() . ')</h2>') ;
-    if(isset($_POST['desc_submit'])) 
-    {
-      $person->setBeschreibung($_POST['beschreibung']) ;
-      if($person->personBeschreibungSave())
-        echo('Speichern erfolgreich') ;
-      else
-        echo('ein Fehler ist aufgetreten') ;
-    }
-    else
-    {
-      echo('<div class="skh3-form">') ;
-      echo('<form action="" method="post">') ;
-      if($func == 'descAdd') //Wenn neuer Eintrag, Menu für Sprachen anzeigen
-      {
-        echo('<label for="sprache">Sprache</label><select name="lang_id" id="sprache">') ;
-        echo('<option value="">Sprache wählen</option>') ;
-        //Sprachen aus DB holen
-        $sql = rex_sql::factory() ;
-        $queryClang = 'SELECT id, name FROM ' . \rex::getTablePrefix() . 'clang;' ;
-        $sql->setQuery($queryClang) ;
-        for($i = 0; $i < $sql->getRows(); $i++)
-        {
-          echo '<option value="' . $sql->getValue('id') .'">' . $sql->getValue('name') .'</option>' ;
-          $sql->next();
-        }
-        echo('</select> <br />');
-      }
-      echo('<label for="beschreibung">Beschreibung</label><textarea class="txt" name="beschreibung" id="beschreibung" cols="50" rows="5">' . $person->getBeschreibung() . '</textarea><br />') ;
-      echo('<input type="submit" class="btn" name="desc_submit" value="Speichern" />') ;
-      echo('</form></div></div>') ;
-    }
-  }
-  //Beschreibung löschen
-  if($func == 'descDel')
-  {
-    $person = new skh3\personen($person_id, $lang_id);
-    $person->beschreibungDelete();
   }
 ?>
